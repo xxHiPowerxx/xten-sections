bed0d6acd7e2339aacf2d0878a17ffe1dba54b58
Ricky Adams <ra@main.local>
2020-04-14 15:09:23 -0700

    Added Build task

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,17 @@ var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
+var del = require("del");
+
+// Configuration file to keep your code DRY
+var cfg = require("./gulpconfig.json");
+var paths = cfg.paths;
 
 // Styles/CSS
-gulp.task('styles', function(done) {
-	gulp
+gulp.task('styles', function (done) {
+	var stream = gulp
 		.src([
-			'./src/sass/**/*.scss',
+			`${paths.sass}/*.scss`,
 			'!src/sass/import/', //exclude import'
 			'!src/sass/import/**/*'
 		])
@@ -31,13 +36,13 @@ gulp.task('styles', function(done) {
 			})
 		)
 		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest('./assets/css/'));
+		.pipe(gulp.dest(paths.css));
 
-	done();
+	return stream;
 });
 
 // Scripts/JS
-gulp.task('scripts', function(done) {
+gulp.task('scripts', function (done) {
 	gulp
 		.src([
 			'./src/js/**/*.js',
@@ -52,7 +57,7 @@ gulp.task('scripts', function(done) {
 
 // Images
 var imgDest = './assets/images/';
-gulp.task('images', function(done) {
+gulp.task('images', function (done) {
 	gulp
 		.src('./src/images/**/*')
 		.pipe(newer(imgDest))
@@ -62,16 +67,64 @@ gulp.task('images', function(done) {
 	done();
 });
 
-function build() {
-	gulp.parallel('styles', 'scripts', 'images')();
-}
-gulp.task('build', function(done) {
-	build();
-	done();
-});
-
-gulp.task('watch', function() {
+gulp.task('watch', function () {
 	gulp.watch('./src/sass/**/*.scss', gulp.series('styles'));
 	gulp.watch('./src/js/**/*.js', gulp.series('scripts'));
 	gulp.watch('./src/images/**/*', gulp.series('images'));
 });
+
+
+// Run:
+// gulp watch-bs
+// Starts watcher with browser-sync. Browser-sync reloads page automatically on your browser
+// gulp.task("watch", gulp.parallel("watch", "scripts", "styles", "imagemin"));
+// Run:
+// Deleting any file inside the /dist folder
+gulp.task("clean-dist", function () {
+	return del([`${paths.dist}/**`]);
+});
+
+// Deleting any file inside the /src folder
+gulp.task("clean-assets", function () {
+	return del(["assets/**/*"]);
+});
+
+// Run
+// gulp dist
+// Copies the files to the /dist folder for distribution as simple theme
+gulp.task(
+	"create-dist",
+	gulp.series("clean-dist", function copyToDistFolder() {
+		const ignorePaths = [
+			`!${paths.node}`,
+			`!${paths.src}`,
+			`!${paths.dist}`,
+			`!${paths.dist}/**`
+		],
+			ignoreFiles = [
+				"!package.json",
+				"!package-lock.json",
+				"!gulpfile.js",
+				"!gulpconfig.json",
+				"!jshintignore",
+				"!.gitignore"
+			];
+
+		console.log({ ignorePaths, ignoreFiles });
+
+		return gulp
+			.src(["**/*", "assets/**/**.*", "*", ...ignorePaths, ...ignoreFiles], {
+				buffer: false
+			})
+			.pipe(gulp.dest(paths.dist));
+	})
+);
+
+// Build project
+gulp.task(
+	"build",
+	gulp.series(
+		"clean-assets",
+		gulp.series("styles", "scripts", "images", "create-dist")
+	)
+);

233214cfa7659c89dbd702a423ed2a9a0b5076c3
Ricky Adams <ra@main.local>
2020-04-14 15:13:14 -0700

    added custom event for load-splash in xten main.js

--- src/js/xten-section-hero.js
+++ src/js/xten-section-hero.js
@@ -1,7 +1,16 @@
 (function ($) {
 	$(document).on("ready", function () {
+		function startWork(elem) {
+			window.workStarted = window.workStarted || {};
+			window.workStarted[elem] = true;
+		}
+		function finishWork(elem) {
+			delete window.workStarted[elem];
+			window.dispatchEvent(new CustomEvent('finishWork'));
+		}
 		function sizeHero() {
 			$('.sizeHero').each(function () {
+				startWork(this);
 				var section = $(this).closest('.xten-section'),
 					minHeight = section.attr('data-minimum-height'),
 					viewPortHeight = window.innerHeight,
@@ -16,10 +25,50 @@
 					minHeightPercent = minHeightNum / 100,
 					calculatedHeight = spaceAvailable * minHeightPercent;
 				$(this).css('min-height', calculatedHeight + 'px');
+				finishWork(this);
+			});
+		}
+		function isInViewport(elem) {
+			if (elem instanceof jQuery) {
+				elem = elem[0];
+			}
+			var bounding = elem.getBoundingClientRect();
+			return (
+				bounding.top >= 0 &&
+				bounding.left >= 0 &&
+				bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+				bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+			);
+		};
+		// Polyfill for startsWith.
+		if (!String.prototype.startsWith) {
+			String.prototype.startsWith = function (searchString, position) {
+				position = position || 0;
+				return this.indexOf(searchString, position) === position;
+			};
+		}
+		function listenForImageLoad() {
+			$('.xten-section-hero').each(function () {
+				if (isInViewport(this)) {
+					var backgroundImage = getComputedStyle(this).backgroundImage;
+					if (backgroundImage.startsWith('url')) {
+						startWork(this);
+						var src = backgroundImage.replace('url(', ''),
+							src = src.replace('"', ''),
+							src = src.replace(')', ''),
+							src = src.replace('"', ''),
+							that = this;
+						$('<img/>').attr('src', src).on('load', function () {
+							$(this).remove();
+							finishWork(that);
+						});
+					} // endif (backgroundImage.startsWith('url'))
+				}// endif ( isInViewport(this) )
 			});
 		}
 		function readyFuncs() {
 			sizeHero();
+			listenForImageLoad();
 		}
 		readyFuncs();
 		function resizeFuncs() {
